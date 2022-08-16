f1ba66c8c508f7b4e33dd91e72b7b47410c306c3
Elvis-Munene <gitau.elvis@student.moringaschool.com>
2022-08-16 17:00:15 +0300

    commit tree 33a81698e62bb0b49f5c237f4465f0f5fd1b8718

---
+++ app.rb
@@ -0,0 +1,5 @@
+def my_Function(param)
+    puts "Hey do this math"
+    param * 3
+    end
+    
--- methods.rb
+++ methods.rb
@@ -1,1 +1,36 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+
+greet_programmer
+
+
+def greet(name )
+    puts "Hello, #{name}!"
+end
+greet("Naureen")
+greet("Jimmy")
+
+def greet_with_default(name="programmer")
+    puts "Hello, #{name}!"
+end 
+greet_with_default
+
+
+
+def add(num1,num2)
+    return num1+num2
+end
+
+add(1,2)
+
+def halve(num)
+    return nil unless num.class == Integer 
+
+    num / 2
+
+end
+halve(6)
+halve(1)
